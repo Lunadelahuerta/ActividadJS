9200e5809d8c6266b1bcc99a2b03807e30f1e277
Luna <luna.huerta.c@gmail.com>
2017-04-05 20:06:24 -0300

    cambios

--- actividad.js
+++ actividad.js
@@ -6,7 +6,7 @@ var Resta=Number(Numero1) - Number(Numero2)
 var Multiplicacion=Number(Numero1) * Number(Numero2)
 var Division=Number(Numero1) / Number(Numero2)
 
-alert(Suma+" esto pasa si sumas ambos números");
-alert(Resta+" esto pasa si los restas");
-alert(Multiplicacion+" ...si los multiplicas");
-alert(Division+" o si los divides");
+alert(Suma + " esto pasa si sumas ambos números");
+alert(Resta + " esto pasa si los restas");
+alert(Multiplicacion + " ...si los multiplicas");
+alert(Division + " o si los divides");
