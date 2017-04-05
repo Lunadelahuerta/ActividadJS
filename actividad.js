ad4f1d442f9d6a8eebd2c57a8e00ac1d771fe4ee
Luna <luna.huerta.c@gmail.com>
2017-04-05 19:53:12 -0300

    finaliza actividad

---
+++ actividad.js
@@ -0,0 +1,12 @@
+
+var Numero1= prompt("Ingresa un número al azar :)")
+var Numero2= prompt("Por favor, ahora ingresa otro número")
+var Suma=Number(Numero1) + Number(Numero2)
+var Resta=Number(Numero1) - Number(Numero2)
+var Multiplicacion=Number(Numero1) * Number(Numero2)
+var Division=Number(Numero1) / Number(Numero2)
+
+alert(Suma+" esto pasa si sumas ambos números");
+alert(Resta+" esto pasa si los restas");
+alert(Multiplicacion+" ...si los multiplicas");
+alert(Division+" o si los divides");
